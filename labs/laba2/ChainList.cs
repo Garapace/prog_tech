d4bb3f48373543edf418d39b6a11f2cf9aea766f
Garapace <starkov-2003@list.ru>
2024-04-22 22:02:11 +0500

    доделал зачётку для 2 лабы

---
+++ labs/laba2/ChainList.cs
@@ -0,0 +1,192 @@
+﻿using System.Net.NetworkInformation;
+
+namespace laba2
+{
+    internal class ChainList : BaseList
+    {
+        private class Node
+        {
+            public int Data { set; get; }
+            public Node Next { set; get; }
+
+            public Node(int data, Node next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+
+        private Node head;
+
+        public ChainList()
+        {
+            head = null;
+            quantity = 0;
+        }
+
+        protected override BaseList EmptyClone()
+        {
+            ChainList ChainList = new ChainList();
+            return ChainList;
+        }
+
+        private Node Find(int index)
+        {
+            if (index < 0 || index >= quantity) { return null; }
+
+            int i = 0;
+            Node CurrentNode = head;
+            while (CurrentNode != null && i < index)
+            {
+                CurrentNode = CurrentNode.Next;
+                i++;
+            }
+
+            if (i == index) { return CurrentNode; }
+            else { return null; }
+        }
+
+        public override void Add(int digit)
+        {
+            Node NewNode = new Node(digit, null);
+
+            if (head == null) { head = NewNode; } // проверка что голова не ведёт в null
+            else
+            {
+                Node tail = Find(quantity - 1);
+                tail.Next = NewNode;
+            }
+            quantity++;
+        }
+
+        public override void Insert(int digit, int index)
+        {
+            if (index < 0 || index > quantity) { return; }
+            if (index == 0)
+            {
+                Node NewNode = new Node(digit, null);
+                NewNode.Next = head;
+                head = NewNode;
+            }
+            else
+            {
+                Node CurrentNode = Find(index - 1);
+                Node NewNode = new Node(digit, null);
+                NewNode.Next = CurrentNode.Next;
+                CurrentNode.Next = NewNode;
+            }
+            quantity++;
+        }
+
+        public override void Delete(int index)
+        {
+            if (index < 0 || index >= quantity) { return; }
+            if (index == 0 && head != null)
+            {
+                head = head.Next;
+                quantity--;
+                return;
+            }
+
+            Node PreviousNode = Find(index - 1);
+            Node CurrentNode = Find(index);
+
+            if (CurrentNode != null)
+            {
+                PreviousNode.Next = CurrentNode.Next;
+                quantity--;
+            }
+        }
+
+        public override void Clear()
+        {
+            head = null;
+            quantity = 0;
+        }
+
+        public override int this[int index]
+        {
+            get
+            {
+                if (index < quantity || index >= 0)
+                {
+                    Node CurrentNode = Find(index);
+                    return CurrentNode.Data;
+                }
+                return 0;
+            }
+            set
+            {
+                if (index < quantity || index >= 0)
+                {
+                    Node CurrentNode = Find(index);
+                    CurrentNode.Data = value;
+                }
+            }
+        }
+
+        public override void Sort()
+        {
+            Node CurrentNode = head;
+            for (int i = 0; i < quantity - 1; i++)
+            {
+                Node NextNode = CurrentNode.Next;
+                for (int j = i + 1; j < quantity; j++)
+                {
+                    if (CurrentNode.Data > NextNode.Data)
+                    {
+                        int temp = CurrentNode.Data;
+                        CurrentNode.Data = NextNode.Data;
+                        NextNode.Data = temp;
+                    }
+                    NextNode = NextNode.Next;
+                }
+                CurrentNode = CurrentNode.Next;
+            }
+        }
+
+
+        public override void DeleteRepeat()
+        {
+            Node PrevNode = null;
+            Node CurrentNode = head;
+            while (CurrentNode.Next != null)
+            {
+                Node NextNode = CurrentNode.Next;
+                while (NextNode != null)
+                {
+                    if (CurrentNode.Data == NextNode.Data)
+                    {
+                        if (PrevNode == null)
+                        {
+                            head = head.Next;
+                            quantity--;
+                        }
+                        else
+                        {
+                            PrevNode.Next = CurrentNode.Next;
+                            quantity--;
+                        }
+                        CurrentNode = CurrentNode.Next;
+                        NextNode = CurrentNode;
+                    }
+                    NextNode = NextNode.Next;
+                }
+                PrevNode = CurrentNode;
+                CurrentNode = CurrentNode.Next;
+            }
+        }
+
+
+        public override void Print()
+        {
+            Node CurrentNode = head;
+            while (CurrentNode != null)
+            {
+                Console.Write(CurrentNode.Data + " ");
+                CurrentNode = CurrentNode.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ labs/laba2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseList ArrayList = new ArrayList();
+            BaseList ChainList = new ChainList();
+            Random random = new Random();
+
+            /*for (int i = 0; i < 10; i++)
+            {
+                ChainList.Add(random.Next(1, 6));
+            }*/
+
+            ChainList.Add(1);
+            ChainList.Add(2);
+            ChainList.Add(4);
+            ChainList.Add(3);
+            ChainList.Add(1);
+            ChainList.Add(2);
+            ChainList.Add(3);
+            ChainList.Print();
+            
+            ChainList.DeleteRepeat();
+            ChainList.Print();
+
+
+
+            /*for (int i = 0; i < 5000; i++)
+            {
+                int operation = random.Next(0, 3);
+                int digit = random.Next(0, 1000);
+                int index = random.Next(0, 100);
+
+                switch (operation)
+                {
+                    case 0:
+                        ArrayList.Add(digit);
+                        ChainList.Add(digit);
+                        break;
+                    case 1:
+                        ArrayList.Delete(index);
+                        ChainList.Delete(index);
+                        break;
+                    case 2:
+                        ArrayList.Insert(digit, index);
+                        ChainList.Insert(digit, index);
+                        break;
+                }
+            }
+
+            ArrayList.Sort();
+            BaseList CloneList1 = ArrayList.Clone();
+
+            BaseList CloneList2 = new ChainList();
+            CloneList2.Assign(ChainList);
+            CloneList2.Sort();
+
+            if (CloneList1.IsEqual(CloneList2))
+            {
+                Console.WriteLine("Accept");
+            }
+            else
+            {
+                Console.WriteLine("Bugged");
+            }
+
+
+            for (int i = 0; i < CloneList1.Count; i++)
+            {
+                if (CloneList1[i] != CloneList2[i])
+                {
+                    Console.WriteLine("Error contained in\n" +
+                                    "Array List: " + CloneList1[i] + "\n" +
+                                    "Chain List: " + CloneList2[i] + "\n" +
+                                    "On " + i + " iteration");
+                }
+                else
+                {
+                    Console.WriteLine("Done.\n" +
+                                    "Array List length - \t" + CloneList1.Count + "\n" +
+                                    "Chain List length - \t" + CloneList2.Count + "\n");
+                    break;
+                }
+            }*/
+        }
+    }
+}
