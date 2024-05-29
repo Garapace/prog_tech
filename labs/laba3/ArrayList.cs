dd845cdbdc86ac53164f1c8085b8997be54fbebb
Garapace <starkov-2003@list.ru>
2024-05-29 09:24:39 +0500

    нафака 3 лабу

---
+++ labs/laba3/ArrayList.cs
@@ -0,0 +1,133 @@
+﻿using laba3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class ArrayList<T> : BaseList<T> where T : IComparable<T>
+    {
+        private T[] buffer;
+        public ArrayList() 
+        {
+            quantity = 0;
+            buffer = new T[4];
+        }
+
+        private void Expanding() // метод расширения списка
+        {
+            if (buffer == null) // если список пустой, добавляем ему одну ячейку
+            {
+                buffer = new T[1];
+                return;
+            }
+
+            if (buffer.Length != quantity) // если длина списка не равна кол-ву элементов в этом списке, то выходим из метода расширения
+            {
+                return;
+            }
+
+            // если предыдущие if'ы не сработали, то выполняется код ниже
+
+            T[] temp = new T[quantity * 2]; // создаём временный список
+            for (int i = 0; i < quantity; i++)
+            {
+                temp[i] = buffer[i]; // присваиваем элементы списка buffer во временный массив
+            }
+            buffer = temp; // приравниваем основной список к временному
+        }
+
+        public override void Add(T item) // метод добавления элемента в конец списка, на вход подаётся число
+        {
+            if (buffer.Length == quantity) { Expanding(); }
+            buffer[quantity] = item; // в список в ячейку с индексом quantity (кол-во элементов в это списке) присваивается поданное на вход метода число
+            quantity++; // увеличение кол-ва эл-тов на один
+            check();
+        }
+
+        public override void Insert(T item, int index) // метод вставки элемента в любое существующее место списка
+        {
+            if (index < 0 || index > quantity) { return; } // если индекс превышает кол-во эл-тов массива, то выходим из метода
+
+            if (index == quantity) { Add(item); return; } // если поданный индекс совпадает с кол-вом эл-ов, то идём в метод Add()
+
+            if (buffer.Length == quantity) { Expanding(); }
+
+            for (int i = quantity; i > index; i--)
+            {
+                buffer[i] = buffer[i - 1];
+            }
+            buffer[index] = item;
+            quantity++;
+            check();
+        }
+
+        public override void Delete(int index) // метод удаление элемента списка по его индексу
+        {
+            if (index < 0 || index >= quantity) { return; } // проверка на выход за диапазон списка
+
+            for (int i = index; i < quantity - 1; i++) // удаление элемента
+            {
+                buffer[i] = buffer[i + 1];
+            }
+            quantity--;
+            check();
+        }
+
+        public override void Clear() // метод очиски списка
+        {
+            quantity = 0;
+        }
+
+
+        public override T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < quantity) // если хотим получить элемент, находящийся вне диапазона, то возвращаем нуль
+                {
+                    return buffer[index];
+                }
+                throw new Exceptions.BadIndexException("Не существующий индекс");
+            }
+
+            set
+            {
+                if (index >= 0 || index < quantity)
+                {
+                    buffer[index] = value;
+                    return;
+                }
+                throw new Exceptions.BadIndexException("Не существующий индекс");
+            }
+        }
+
+        public override void Print() // метод вывода списка в консоль
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.Write(buffer[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        protected override BaseList<T> EmptyClone()
+        {
+            return new ArrayList<T>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder write = new StringBuilder();
+            for (int i = 0; i < quantity; i++)
+            {
+                write.Append(buffer[i] + "\n");
+            }
+
+            return write.ToString();
+        }
+    }
+}
---
+++ labs/laba3/BaseList.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+
+namespace laba3
+{
+    abstract class BaseList<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        protected int quantity;
+        public int Count { get { return quantity; } }
+
+        // объявление делегата
+        public delegate void listener();
+        // объявление события
+        public event listener active;
+        // вызов события
+        protected void check()
+        {
+            // ? - оператор проверки на null, ситуация возможна, если нет подписчиков события
+            active?.Invoke();
+        }
+        public abstract void Add(T item);
+        public abstract void Insert(T item, int index);
+        public abstract void Delete(int index);
+        public abstract void Clear();
+        public abstract void Print();
+        public abstract T this[int i] { set; get; }
+
+
+        public void Assign(BaseList<T> source)
+        {
+            Clear();
+            for (int i = 0; i < source.Count; i++)
+            {
+                Add(source[i]);
+            }
+        }
+
+        public void AssignTo(BaseList<T> dest)
+        {
+            dest.Assign(this);
+        }
+
+        protected abstract BaseList<T> EmptyClone();
+        public BaseList<T> Clone()
+        {
+            BaseList<T> clone = EmptyClone();
+            clone.Assign(this);
+            return clone;
+        }
+        public virtual void Sort()
+        {
+            int left = 0;
+            int right = quantity - 1;
+
+            while (left < right)
+            {
+                bool swap = false;
+                for (int i = left; i < right; i++)
+                {
+                    if (this[i].CompareTo(this[i + 1]) > 0)
+                    {
+                        (this[i], this[i + 1]) = (this[i + 1], this[i]);
+                        swap = true;
+                    }
+                }
+
+                if (!swap)
+                {
+                    break;
+                }
+
+                right--;
+
+
+                for (int i = right; i > left; i--)
+                {
+                    if (this[i - 1].CompareTo(this[i]) > 0)
+                    {
+                        (this[i - 1], this[i]) = (this[i], this[i - 1]);
+                    }
+                }
+                left++;
+            }
+        }
+
+        public virtual void DeleteRepeat()
+        {
+            if (quantity == 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < quantity; i++)
+            {
+                bool check = true;
+                for (int j = i + 1; j < quantity; j++)
+                {
+                    if (this[i].CompareTo(this[j]) == 0)
+                    {
+                        Delete(j);
+                        check = false;
+                        j--;
+                    }
+                }
+                if (!check)
+                {
+                    Delete(i);
+                    i--;
+                }
+            }
+        }
+
+        public bool IsEqual(BaseList<T> list)
+        {
+            if (quantity != list.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < quantity; i++)
+            {
+                if (this[i].CompareTo(list[i]) != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public abstract string ToString();
+        public void SaveToFile(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.Write(this.ToString());
+            }
+        }
+        public void loadToFile(string path)
+        {
+            this.Clear();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                try
+                {
+                    string str = reader.ReadToEnd();
+
+                    string[] list = str.Split("\n");
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        string num = list[i].Trim();
+                        if (!string.IsNullOrEmpty(num))
+                        {
+                            T conventNum = (T)Convert.ChangeType(num, typeof(T));
+                            this.Add(conventNum);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw new Exceptions.BadFileException("Некорректный формат данных в файле");
+                }
+            }
+        }
+
+        public static BaseList<T> operator +(BaseList<T> list_1, BaseList<T> list_2)
+        {
+            BaseList<T> merged = list_1.Clone();
+            for (int i = 0; i < list_2.Count; i++)
+            {
+                merged.Add(list_2[i]);
+            }
+            return merged;
+        }
+        public static bool operator ==(BaseList<T> list_1, BaseList<T> list_2)
+        {
+            if (list_1.IsEqual(list_2)) return true;
+            else return false;
+        }
+        public static bool operator !=(BaseList<T> list_1, BaseList<T> list_2)
+        {
+            if (list_1.IsEqual(list_2)) return false;
+            else return true;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new EnumList(this);
+        }
+
+        protected class EnumList : IEnumerator<T>
+        {
+            private BaseList<T> list;
+            private int index;
+
+            public EnumList(BaseList<T> list)
+            {
+                this.list = list;
+                this.index = -1;
+            }
+
+            object IEnumerator.Current { get { return Current; } }
+            public bool MoveNext()
+            {
+                if (index < list.quantity - 1)
+                {
+                    index++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset() { index = -1; }
+
+            public void Dispose() { }
+
+            public T Current
+            {
+                get { return list[index]; }
+            }
+        }
+    }
+}
